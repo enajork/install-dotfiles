d87113ccaf4a7ea25d5fdf5693c90bb3479968cc
Eric Najork <eric@najork.org>
2025-02-02 14:18:56 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sL https://raw.githubusercontent.com/enajork/install_dotfiles/main/install.sh -o install.sh && \
-curl -sL https://raw.githubusercontent.com/enajork/install_dotfiles/main/playbook.yaml -o playbook.yaml
+curl -sL https://raw.githubusercontent.com/enajork/install-dotfiles/main/install.sh -o install.sh && \
+curl -sL https://raw.githubusercontent.com/enajork/install-dotfiles/main/playbook.yaml -o playbook.yaml
 chmod +x install.sh
 ./install.sh
 rm -f install.sh playbook.yaml
